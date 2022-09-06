d7c4d16293b4d645a79b1f09735fd823a7a1a607
liormw <102317697+liormw@users.noreply.github.com>
2022-09-06 18:28:44 +0300

    Update afile.py

--- afile.py
+++ afile.py
@@ -10,3 +10,11 @@ subprocess.Popen('/bin/echo', shell=True)
 
 
 subprocess.Popen('/bin/echo', shell=True)
+
+
+
+subprocess.Popen('/bin/echo', shell=True)
+
+
+
+subprocess.Popen('/bin/echo', shell=True)
