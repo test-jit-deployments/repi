7e1d808f59d5caf9673529cccc3ce0985d5d8f6a
liormw <102317697+liormw@users.noreply.github.com>
2022-09-18 11:22:18 +0300

    Update afile.py

--- afile.py
+++ afile.py
@@ -1,18 +1,4 @@
 import subprocess
 
-subprocess.Popen('/bin/echo', shell=True)
-
-
-subprocess.Popen('/bin/echo', shell=True)
-
-
-
-subprocess.Popen('/bin/echo', shell=True)
-
-
-
-subprocess.Popen('/bin/echo', shell=True)
-
-
 
 subprocess.Popen('/bin/echo', shell=True)
